ed530563aa6b575b7638bfcf327fbab5139b4a67
Mahesh Lanka <mlanka@codeaurora.org>
2014-06-11 15:32:54 +0530

    mm-dash: Remove target specific compilation
    Remove target specific compilation
    checks in makefile
    
    Change-Id: I0e4214c603338e0d99e0e827725b251d45a66176

--- dashplayer/Android.mk
+++ dashplayer/Android.mk
@@ -51,9 +51,7 @@ ifeq ($(PLATFORM_SDK_VERSION), 18)
   LOCAL_CFLAGS += -DANDROID_JB_MR2
 endif
 
-ifneq ($(filter msm8974 msm8960 msm8226 apq8084 mpq8092 msm8610 msm_bronze msm8916_32 msm8916_32_512,$(TARGET_PRODUCT)),)
-  LOCAL_CFLAGS += -DBFAMILY_TARGET
-endif
+LOCAL_CFLAGS += -DBFAMILY_TARGET
 
 LOCAL_MODULE:= libdashplayer
 
