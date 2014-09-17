cf9166e1071dfd2cd82ed74a86edea94ddda0e2c
Tom Marshall <tdm@cyngn.com>
2014-09-17 15:51:17 -0700

    mm-dash: Use project pathmap
    Change-Id: Iec04ea186cd149310689e19fbaf19ce57b02bd38

--- dashplayer/Android.mk
+++ dashplayer/Android.mk
@@ -36,8 +36,8 @@ LOCAL_C_INCLUDES := \
 	$(TOP)/frameworks/av/media/libstagefright/include             \
 	$(TOP)/frameworks/av/media/libstagefright/mpeg2ts             \
 	$(TOP)/frameworks/av/media/libstagefright/rtsp                \
-	$(TOP)/hardware/qcom/media/mm-core/inc                        \
-	$(TOP)/hardware/qcom/display/libgralloc                       \
+	$(TOP)/$(call project-path-for,qcom-media)/mm-core/inc        \
+	$(TOP)/$(call project-path-for,qcom-display)/libgralloc       \
         $(LOCAL_PATH)/..                                            \
 	$(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 
