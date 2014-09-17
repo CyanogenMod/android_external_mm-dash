0a29c63d30cc48494162e6775dadccf32d6ffdb9
Tom Marshall <tdm@cyngn.com>
2014-09-17 15:51:17 -0700

    mm-dash: Use project pathmap
    Change-Id: Iec04ea186cd149310689e19fbaf19ce57b02bd38

--- Android.mk
+++ Android.mk
@@ -1,5 +1,9 @@
+# This project has been split from caf media as of caf-kk3.10
+ifeq ($(TARGET_QCOM_MEDIA_VARIANT),caf-kk3.10)
+
 QCOM_MEDIA_ROOT := $(call my-dir)
 
 include $(QCOM_MEDIA_ROOT)/QCMediaPlayer/Android.mk
 include $(QCOM_MEDIA_ROOT)/dashplayer/Android.mk
 
+endif
